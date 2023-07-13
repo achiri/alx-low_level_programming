62810429fe6b9f7c52736e8aadc1411da5a3c3e0
achiri <tanguachiri@gmail.com>
2023-07-13 14:38:39 -0700

    Printing without printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 /*This program prints a string without the printf function*/
-#include <stdio.h>
-/**
-*Hello World  
-*/
+#include<stdio.h>
+/*Hello World*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	/*return void*/
 	return (0);
 }
