71e8907496d4c0f74480ca6fc1f41836e810a440
achiri <tanguachiri@gmail.com>
2023-07-13 15:01:53 -0700

    Print with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - this is the starting point
+* Return: should always return void
+* Description: Users printf to print to output
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
