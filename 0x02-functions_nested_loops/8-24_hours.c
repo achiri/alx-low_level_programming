5ee834cbd928f1784c91d7d24ef8ce852f21edc8
achiri <tanguachiri@gmail.com>
2023-07-18 08:43:13 -0700

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,8 +10,10 @@ void jack_bauer(void)
 {
 	int min, hour;
 	for (hour = 0; hour <= 23; hour++)
+
 	{
 		for (min = 0; min <= 59; min++)
+
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
