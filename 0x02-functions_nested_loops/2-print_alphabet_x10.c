d374da2245cb10eea7ffa21d893a9f2002ee2256
achiri <tanguachiri@gmail.com>
2023-07-18 07:48:43 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,13 @@ void print_alphabet_x10(void)
 {
 	char ch;
 	int count;
-	for(count = 1; count <= 10; count++)
+	for (count = 1; count <= 10; count++)
+	
 	{
-		for(ch = 'a'; ch <= 'z'; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 		_putchar(ch);
 		}
 		_putchar('\n');
-
-	}	
+	}
 }
