41244ac154c56550b4081464c48a7fe846a673eb
achiri <tanguachiri@gmail.com>
2023-07-18 06:47:59 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* main - will use _putchar to print _putchar
+* Return: Always returns void and exits the program
+*/
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
+
