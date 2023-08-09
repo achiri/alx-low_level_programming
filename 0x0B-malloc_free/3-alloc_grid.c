bc4df2362b0f219fc2145fb5583aed99210b46b4
achiri <tanguachiri@gmail.com>
2023-08-08 21:22:41 -0700

    Optional Task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,45 +1,30 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * alloc_grid - returns a pointer to a 2 dimensional array of integers
- * @width: columns
- * @height: rows
- * Return: a pointer to a 2 dimensional array of integers
+ * array_range - create array of integers
+ * @min: start range from
+ * @max: end range at
+ * Return: pointer to array
  */
 
-int **alloc_grid(int width, int height)
+int *array_range(int min, int max)
 {
-	int **grid, row, col;
+	int *ptr;
+	int i;
+	int n = (max - min + 1);
 
-	if (width < 1 || height < 1)
+	if (min > max) /* validate input */
 		return (NULL);
 
-	grid = (int **)malloc(height * sizeof(int *));
-
-	if (!grid)
-	{
-		free(grid);
+	/* malloc and check for error */
+	ptr = malloc(sizeof(int) * n);
+	if (ptr == NULL)
 		return (NULL);
-	}
 
-	for (row = 0; row < height; row++)
-	{
-		grid[row] = (int *)malloc(width * sizeof(int));
+	/* set values */
+	for (i = 0; i < n; i++)
+		ptr[i] = min++;
 
-		if (!grid[row])
-		{
-			while (row)
-			{
-				free(grid[row]);
-				row--;
-			}
-			free(grid);
-			return (NULL);
-		}
-		for (col = 0; col < width; col++)
-			grid[row][col] = 0;
-	}
-
-	return (grid);
+	return (ptr);
 }
